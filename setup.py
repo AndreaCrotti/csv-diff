fbf22ef57dd79a2b6530187eb233c79a038f6b48
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-10-11 14:34:40 +0100

    mark the file as a running script

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(name=package, version=version,
       description="show differences between csv files",
       setup_requires=setup_requires,
       install_requires=setup_requires,
+      scripts=['csv_diff.py'],
       author='Andrea Crotti', author_email='andrea.crotti.0@gmail.com',
       url='url'
 )
