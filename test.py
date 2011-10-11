1e4b4dc41793921ab648482007ba6a7f1989880a
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-10-11 16:09:36 +0100

    add compute_diff tests

--- test.py
+++ test.py
@@ -8,10 +8,21 @@ class TestCsvDiff(unittest.TestCase):
             0: 'A',
             3: 'D',
             26: 'AA',
-            52: 'BA'}
+            52: 'BA'
+        }
 
         for val, res in idx_to_st.items():
             self.assertEqual(csvdiff.index_to_excel(val), res)
+
+    def test_diffs(self):
+        diffs = {
+            (1, 1.1): 0.1,
+            (0, 0): 0,
+            (-1, 2): 3
+        }
+
+        for vals, diff in diffs.items():
+            self.assertAlmostEqual(csvdiff.compute_diff(*vals), diff)
 
 
 if __name__ == '__main__':
