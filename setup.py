f055809e0a7713c71b31387063bf1dfa2d04fb24
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-10-11 14:30:17 +0100

    add a setup.py which adds argparse to setup_requires and install_requires if failing to import

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+package = 'csv-diff'
+version = '0.1'
+
+setup_requires = []
+
+try:
+    import argparse
+except ImportError:
+    setup_requires.append('argparse')
+
+setup(name=package, version=version,
+      description="show differences between csv files",
+      setup_requires=setup_requires,
+      install_requires=setup_requires,
+      author='Andrea Crotti', author_email='andrea.crotti.0@gmail.com',
+      url='url'
+)
